3675f79708503bc05f47879272325c25aee59bb8
Weiyi Wang <wwylele@gmail.com>
2019-09-11 22:14:42 -0400

    hmm

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -ex
 
-openssl aes-256-cbc -K $encrypted_e850a6789831_key -iv $encrypted_e850a6789831_iv -in index3ds-deploy-key.enc -out target/index3ds-deploy-key -d
-chmod 600 target/index3ds-deploy-key
-ssh-add target/index3ds-deploy-key
-ssh ubuntu@index3ds.com 'bash -s' < /home/ubuntu/index3ds/after-deploy.sh
+openssl aes-256-cbc -K $encrypted_e850a6789831_key -iv $encrypted_e850a6789831_iv -in index3ds-deploy-key.enc -out index3ds-deploy-key -d
+chmod 600 index3ds-deploy-key
+ssh-add index3ds-deploy-key
+ssh ubuntu@index3ds.com '/home/ubuntu/index3ds/after-deploy.sh'
