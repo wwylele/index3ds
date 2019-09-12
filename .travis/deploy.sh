ddd5cba6e9c5f52150ee574241f85e18a3c9d206
Weiyi Wang <wwylele@gmail.com>
2019-09-11 23:02:12 -0400

    another try

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -10,8 +10,7 @@ tar -cvzf deploy.tar.gz deploy
 openssl aes-256-cbc -K $encrypted_e850a6789831_key -iv $encrypted_e850a6789831_iv -in index3ds-deploy-key.enc -out index3ds-deploy-key -d
 chmod 600 index3ds-deploy-key
 
-curl --ftp-create-dirs -T deploy.tar.gz --key index3ds-deploy-key sftp://ubuntu@index3ds.com/index3ds/deploy.tar.gz
-
 eval "$(ssh-agent -s)"
 ssh-add index3ds-deploy-key
-ssh ubuntu@index3ds.com '/home/ubuntu/index3ds/after-deploy.sh' -o "StrictHostKeyChecking no"
+scp -o "StrictHostKeyChecking no" deploy.tar.gz ubuntu@index3ds.com:/home/ubuntu/index3ds
+ssh -o "StrictHostKeyChecking no" ubuntu@index3ds.com '/home/ubuntu/index3ds/after-deploy.sh'
