c46aa9ec29409b5c1124d72c218b7cc1e27d3587
Weiyi Wang <wwylele@gmail.com>
2019-09-11 21:49:18 -0400

    try deploy script

---
+++ .travis/deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+openssl aes-256-cbc -K $encrypted_e850a6789831_key -iv $encrypted_e850a6789831_iv -in index3ds-deploy-key.enc -out index3ds-deploy-key -d
+chmod 600 index3ds-deploy-key
+ssh-add index3ds-deploy-key
+ssh ubuntu@index3ds.com 'bash -s' < /home/ubuntu/index3ds/after-deploy.sh
